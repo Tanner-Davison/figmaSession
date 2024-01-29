996d07e4c1671ffaf455a7f951c54e7444c40bff
Tanner Davison <Tboydavison@gmail.com>
2024-01-29 14:12:31 -0700

    completed

---
+++ src/components/GlobalComponents/FooterDummyData.js
@@ -0,0 +1,15 @@
+const footerData = [
+    {Header: "COMPANY",
+        Links: ["About Us", "Careers", "Giving"]
+    },
+    {
+    Header: "PARTNERS",
+        Links: ["Become A Partner", "Partner Overview","MSP Partners"]
+    },
+    {
+    Header: "RESOURCES",
+        Links: ["Blog", "Recourse Library", "Support Center", " MSLA Agreement"]
+    }
+]
+
+export default footerData
