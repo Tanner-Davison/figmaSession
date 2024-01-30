615afce5f66a8805edf9d37e8872d3da410a04c5
Tanner Davison <Tboydavison@gmail.com>
2024-01-29 17:10:19 -0700

    fixed all

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css";
 import Footer from "./GlobalComponents/Footer";
+import SideQuote from "./SideQuote";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,7 +27,7 @@ const Layout = ({ children }) => {
   return (
     <>
     
-
+    <SideQuote/>
    <Footer/>
     </>
   )
