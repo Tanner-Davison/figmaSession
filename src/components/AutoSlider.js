d04f430885676bf49abe4b8082eaf84d59a03f48
Tanner Davison <Tboydavison@gmail.com>
2024-02-06 12:53:50 -0700

    added set Timeout

--- src/components/AutoSlider.js
+++ src/components/AutoSlider.js
@@ -11,54 +11,85 @@ import {
   GlobalLinkButton,
 } from "./buttons/Buttons"
 
-
-
 const AutoSlider = ({ scrollto }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
   const [arrayValue, setArrayValue] = useState([])
+  const [restart, setRestart] = useState(false)
+  const [width, setWidth] = useState(0 + "%")
+  const [width2, setWidth2] = useState(0 + "%")
+  const [width3, setWidth3] = useState(0 + "%")
   const viewboxRef = useRef(null)
-  let cardCount = 0 
-  let transCount = 0;
- let width = 0
+  let cardCount = 0
+  let transCount = 0
 
+  const clearStates = async () => {
+    setWidth(100 + "%")
+    setWidth2(0 + "%")
+    setWidth3(0 + "%")
+    return
+  }
+  const returnBeginning = () => {
+    setRestart(false)
+    setWidth(100 + "%")
+    handleClickRight()
+  }
+  const startTime = async () => {
+    setTimeout(() => {
+      if (transCount === -1) {
+        return returnBeginning()
+      }
+      if (transCount === 0) { // first Transition
+        setWidth2(100 + "%")
+        handleClickRight()
 
- const handleWidth = setInterval(() => {
-   width += 5
-   const myElement = document.getElementById('growme')
-   myElement.style.width = width + "px"
- }, 100)
+      } else if (transCount === 1) { // second transition
+        setWidth3(100 + "%")
+        handleClickRight()
+      }
+      if (transCount === 2) { // third transition back to start 
+        setWidth(0+'%')
+        handleClickRight()
+      }
+      console.log(transCount)
+    }, 4000)
+  }
 
-const startTime = ()=>setInterval(() => {
-  handleClickRight()
+  const handleClickRight = async () => {
+    const target = viewboxRef.current.querySelectorAll(`#cardwrapper`)
+    if (transCount === -1) {
+      transCount = 0
+      return startTime()
+    }
+    if (transCount === 0) {
+      await gsap.to(target, { xPercent: -335, duration: 0.9 })
+      transCount++
+    } else if (transCount === 1) {
+      await gsap.fromTo(
+        target,
+        { xPercent: -335 },
+        { xPercent: -670, duration: 0.9 }
+      )
+      transCount++
 
-}, 4000)
-
- setTimeout(() => {
-   clearInterval(startTime)
-  clearInterval(handleWidth)
- }, 3000)
+    } else if (transCount === 2) {
+      const newTarget = viewboxRef.current.querySelectorAll(`box1`)
+      setRestart(true)
+      clearStates()
+      transCount = -1
+      await gsap.fromTo(
+        target,
+        { xPercent: -670 },
+        { xPercent: 0, duration: 0.9 }
+      )}
+    return startTime()
+  }
 
   useEffect(() => {
     const target = document.getElementById(`#cardwrapper`)
     gsap.set(target, { xPercent: 0 })
-     startTime()
+    startTime()
+    setWidth(100 + "%")
   }, [])
-
-  const handleClickRight = async () => {
-    const target = viewboxRef.current.querySelectorAll(`#cardwrapper`)
-    if(transCount === 0){
-      await gsap.to(target, { xPercent: -335, duration: 0.9 })
-     transCount++
-    }else if(transCount===1){
-      await gsap.fromTo(target, { xPercent: -335 },{xPercent:-670, duration:0.8 })
-     transCount++
-    }else if(transCount ===2){
-      const newTarget = viewboxRef.current.querySelectorAll(`box1`)
-       await gsap.fromTo(target, { xPercent: -670 }, { xPercent: 0, duration: 1 ,ease:'slow' })
-      transCount = 0;
-    }
-    return startTime();
-  }
 
   const runCards = (imgObj, index) => {
     return (
@@ -86,10 +117,15 @@ const startTime = ()=>setInterval(() => {
         </ViewBox>
       </BoxContainer>
       <Controls>
-        <CarouselButtonLeft>Prev</CarouselButtonLeft>
         <StyledData></StyledData>
-        <ButtonCustom id={'growme'} onClick={handleClickRight}>
-          Next
+        <ButtonCustom>
+          <ButtonGrowth id={"growme"} $restart={restart} $width={width} />
+        </ButtonCustom>
+        <ButtonCustom id={"growme"} $width={width2}>
+          <ButtonGrowth id={"growme"} $restart={restart} $width={width2} />
+        </ButtonCustom>
+        <ButtonCustom id={"growme"} $width={width3}>
+          <ButtonGrowth id={"growme"} $restart={restart} $width={width3} />
         </ButtonCustom>
       </Controls>
     </Wrapper>
@@ -97,8 +133,42 @@ const startTime = ()=>setInterval(() => {
 }
 
 export default AutoSlider
+const ButtonGrowth = styled.div`
+  position: relative;
+  display: flex;
+  border-radius: 10px;
+  height: 100%;
+  width: ${props => (props.$width ? `${props.$width}` : `unset`)};
+  transition: ${props => (props.$restart ? "unset" : `width 4s ease-in`)};
+  background-color: white;
+`
 const ButtonCustom = styled.div`
-  height: 20px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  border-radius: 10px;
+  height: 10px;
+  width: 100px;
+  border: 2px solid red;
+`
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  border: 1px groove white;
+  background-color: ${colors.grey};
+  transition: box-shadow 0.3s ease-in-out;
+  margin-top: 1.736vw;
+  padding: 10px;
+  gap: 1.389vw;
+  border-radius: 1vw;
+
+  ${media.mobile} {
+    gap: 1.402vw;
+    border-radius: 3.505vw;
+  }
 `
 const StyledData = styled.p`
   ${text.bodyMBold};
@@ -170,32 +240,6 @@ const Slider = styled.div`
   display: flex;
 `
 
-const Controls = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-  border: 1px groove white;
-  background-color: ${colors.grey};
-  transition: box-shadow 0.3s ease-in-out;
-  box-shadow: 0px 4px 19px ${colors.grey200};
-  gap: 1.389vw;
-  border-radius: 1.389vw;
-  &:hover {
-    border: 1px groove whitesmoke;
-    box-shadow: 0px 4px 19px ${colors.grey200};
-  }
-  ${media.fullWidth} {
-  }
-
-  ${media.tablet} {
-  }
-
-  ${media.mobile} {
-    gap: 1.402vw;
-    border-radius: 3.505vw;
-  }
-`
 const BoxContainer = styled.div`
   display: flex;
   align-items: center;
